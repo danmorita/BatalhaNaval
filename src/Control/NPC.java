549b9ef24d53253145196e0966ea930167baece9
danmorita <danmorita1@gmail.com>
2017-10-17 20:29:09 -0200

    Add files via upload

---
+++ src/Control/NPC.java
@@ -0,0 +1,7 @@
+package Control;
+
+import Model.Jogador;
+
+public class NPC extends Jogador {
+	
+}
---
+++ src/Model/Barco.java
@@ -0,0 +1,23 @@
+package Model;
+
+
+
+public class Barco {
+
+	private String tamanho;
+	private String posicao;
+	private boolean 
+	private static int submarino = 1;
+	private static int navio = 2;
+	private static int encruzador = 3;
+	
+	
+	public Navio(){
+		
+	}
+	
+	public boolean colocarBarco(){
+		
+	}
+	
+}
