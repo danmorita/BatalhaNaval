af9b0f09a16a7b34b71d43e8dce89de83a2fdd4c
pereirapk <ggpereirasv@gmail.com>
2017-11-23 15:21:36 -0200

    concertei erros

--- src/Model/NPC.java
+++ src/Model/NPC.java
@@ -2,16 +2,12 @@ package Model;
 
 import java.util.Random;
 import java.util.Scanner;
-<<<<<<< HEAD
+
 //radom para o bot acerta o barco,criar um vetor onde o bot testa se ele acerta ou erra se
 //for <7 ele erra(facil),se for <5 erra (medio),se for <3 erra(hard)
 import View.Tabuleiro;
 //fazer o bot acerta navios
-=======
 
-import View.Tabuleiro;
-
->>>>>>> refs/remotes/origin/master
 public class NPC {
 	Scanner input = new Scanner(System.in);
 	Random random = new Random();
@@ -25,11 +21,8 @@ public class NPC {
 			{ "G", "\t", "~\t", "~\t", "~\t", "~\t", "~\t", "~\t", "~\t", "~\t", "~\t" },
 			{ "H", "\t", "~\t", "~\t", "~\t", "~\t", "~\t", "~\t", "~\t", "~\t", "~\t" },
 			{ "I", "\t", "~\t", "~\t", "~\t", "~\t", "~\t", "~\t", "~\t", "~\t", "~\t" },
-<<<<<<< HEAD
 			{ "\n-", "-", "-", "Legenda:  ", "~ <- oceano\t", "N <- barcos\t", "X <- erros\t", "ï¿½ <- acertos\t", "B","O", "T\n\n" } };
-=======
-			{ "\n-", "-", "-", "Legenda:  ", "~ <- oceano\t", "N <- barcos\t", "X <- erros\t", "Ñ <- acertos\t", "B","O", "T\n\n" } };
->>>>>>> refs/remotes/origin/master
+
 	
 	
 	String posicao; 
@@ -45,11 +38,9 @@ public class NPC {
 			{ "G", "\t", "~\t", "~\t", "~\t", "~\t", "~\t", "~\t", "~\t", "~\t", "~\t" },
 			{ "H", "\t", "~\t", "~\t", "~\t", "~\t", "~\t", "~\t", "~\t", "~\t", "~\t" },
 			{ "I", "\t", "~\t", "~\t", "~\t", "~\t", "~\t", "~\t", "~\t", "~\t", "~\t" },
-<<<<<<< HEAD
+
 			{ "\n-", "-", "-", "Legenda:  ", "~ <- oceano\t", "N <- barcos\t", "X <- erros\t", "ï¿½ <- acertos\t", "B","O", "T\n\n" } };
-=======
-			{ "\n-", "-", "-", "Legenda:  ", "~ <- oceano\t", "N <- barcos\t", "X <- erros\t", "Ñ <- acertos\t", "B","O", "T\n\n" } };
->>>>>>> refs/remotes/origin/master
+
 	Tabuleiro tabuleiroBotView = new Tabuleiro(tabuView);
 	public void npc() {
 	
@@ -93,11 +84,7 @@ public class NPC {
 		errou = tabuleiroBot.verificaLista(posicao);
 		System.out.println("entrou aqui "+errou);
 		if(errou==false){
-<<<<<<< HEAD
 			tabuleiroBotView.posicionarBarco(posicao, "ï¿½");
-=======
-			tabuleiroBotView.posicionarBarco(posicao, "Ñ");
->>>>>>> refs/remotes/origin/master
 			errou=true;
 		}else if(errou==true){
 			tabuleiroBotView.posicionarBarco(posicao, "X");
